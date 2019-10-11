e059025ac36bdcabc4155401dce1e2fb1d673f17
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 20:46:51 +0300

    Scripted auto-commit on change (2019-10-11 20:46:51) by gitwatch.sh

--- src/main/java/AirportsJoinMapper.java
+++ src/main/java/AirportsJoinMapper.java
@@ -2,6 +2,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
