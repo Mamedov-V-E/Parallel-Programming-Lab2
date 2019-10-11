3621efeedc1bf73659fc8c93234f787b00c415c1
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 21:27:09 +0300

    Scripted auto-commit on change (2019-10-11 21:27:09) by gitwatch.sh

--- src/main/java/TextPair.java
+++ src/main/java/TextPair.java
@@ -59,7 +59,7 @@ public class TextPair implements WritableComparable<TextPair> {
         }
     }
 
-    public static class WritableComparator implements RawComporator<T> {
+    public static class WritableComparator implements RawComparator {
 
     }
 }
