d72c7c420e4e00901893f9d1910984ab73da7bc6
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 21:50:20 +0300

    Scripted auto-commit on change (2019-10-11 21:50:20) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -11,8 +11,11 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
-        Text airportName = new Text(iter.next());
-        Text outValue = 
+        Text outValue = new Text(iter.next());
+
+        IntWritable min = new IntWritable(Integer.MAX_VALUE);
+        IntWritable max = new IntWritable(Integer.MIN_VALUE);
+        
         while (iter.hasNext()) {
             IntWritable delay = new IntWritable(Integer.parseInt(iter.next().toString()));
 
