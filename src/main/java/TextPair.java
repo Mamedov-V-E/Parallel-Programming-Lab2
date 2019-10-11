b185184b5e13dcda765f1989a197b8508ecc7a15
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 20:18:36 +0300

    Scripted auto-commit on change (2019-10-11 20:18:36) by gitwatch.sh

--- src/main/java/TextPair.java
+++ src/main/java/TextPair.java
@@ -3,7 +3,9 @@ import org.apache.hadoop.io.WritableComparable;
 import java.io.DataInput;
 import java.io.DataOutput;
 
-public class TextPair implements WritableComparable {
+public class TextPair implements WritableComparable<TextPair> {
+
+    public void compareTo(TextPair)
 
     public void readFields(DataInput in) {
 
