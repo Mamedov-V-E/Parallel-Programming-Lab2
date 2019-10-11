3b19108970ac6a05ae3228c890c305aa4c1cc698
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 19:33:19 +0300

    Scripted auto-commit on change (2019-10-11 19:33:19) by gitwatch.sh

--- src/main/java/FlightsLogWritable.java
+++ src/main/java/FlightsLogWritable.java
@@ -3,7 +3,9 @@ import org.apache.hadoop.io.Writable;
 import java.io.DataOutput;
 
 public class FlightsLogWritable implements Writable {
-    public void write(DataOutput) {
-        
+
+
+    public void write(DataOutput out) {
+
     }
 }
