47eab8f5ecd5dd391829796e66829929f520b7c7
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 20:46:59 +0300

    Scripted auto-commit on change (2019-10-11 20:46:59) by gitwatch.sh

--- src/main/java/AirportsJoinMapper.java
+++ src/main/java/AirportsJoinMapper.java
@@ -8,7 +8,7 @@ public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Tex
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         ServiceCall call = new ServiceCall(value);
-        context.write(new TextPair(call.getSystemA().toString(),"1"),
+        context.write(new TextPair(call.getSystemA().toString(),"0"),
                 new Text(call.toString()));
     }
 }
