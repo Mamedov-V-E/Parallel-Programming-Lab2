f74ba3e4fc70d0699801ecbf843352d6d75b3a8c
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 20:26:29 +0300

    Scripted auto-commit on change (2019-10-11 20:26:29) by gitwatch.sh

--- src/main/java/TextPair.java
+++ src/main/java/TextPair.java
@@ -10,7 +10,11 @@ public class TextPair implements WritableComparable<TextPair> {
     private IntWritable airportId;
     private BooleanWritable dataSet;
 
-    public IntWritable G
+    public IntWritable getAirportId() {
+        return this.airportId;
+    }
+
+    public void setAirportId()
 
     public int compareTo(TextPair pair) {
 
