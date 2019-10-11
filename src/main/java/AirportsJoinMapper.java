2b145e6d1946794f06e98310f80daf4331d7c9a7
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 20:45:30 +0300

    Scripted auto-commit on change (2019-10-11 20:45:30) by gitwatch.sh

--- src/main/java/AirportsJoinMapper.java
+++ src/main/java/AirportsJoinMapper.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
 }
