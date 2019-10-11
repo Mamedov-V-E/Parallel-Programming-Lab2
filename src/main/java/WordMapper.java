86d45e29a8b17acbe45483333080fdc441ca5d8c
Mamedov-V-E <black_snake666@mail.ru>
2019-10-11 19:22:15 +0300

    Scripted auto-commit on change (2019-10-11 19:22:15) by gitwatch.sh

--- src/main/java/WordMapper.java
+++ src/main/java/WordMapper.java
@@ -10,22 +10,10 @@ public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
             InterruptedException {
 
         String line = value.toString();
-
-//        String specialSymbols = "\\P{L}+";
-//        String specialSymbols = "[^a-zA-Z0-9а-яА-Я'\\- ]";
         String specialSymbols = "[—\\[\\]?!,.«»();„“:…]+";
-//        String specialSymbols = "[\\[\\]?!,.«»();„“:…]|— ";
-//        String words[] =
-//                line.replaceAll(specialSymbols, "").replaceAll("\\n+", " ").toLowerCase().split(" ");
-//        String words[] =
-//                line.replaceAll(specialSymbols, "").toLowerCase().split("\\s+");
         String words[] =
                 line.replaceAll(specialSymbols, "").toLowerCase().split("\\s+");
-//        String words[] =
-//                line.toLowerCase().split(specialSymbols);
-
         for (String word : words) {
-//            context.write(new Text(word), new IntWritable(1));
             if (!word.isEmpty()) {
                 context.write(new Text(word), new IntWritable(1));
             }
