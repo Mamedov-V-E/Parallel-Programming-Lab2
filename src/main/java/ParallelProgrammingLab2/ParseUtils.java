0e1979dea03befc031bba3f9b5291e0637e79727
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 00:53:32 +0300

    Scripted auto-commit on change (2019-10-18 00:53:32) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/ParseUtils.java
+++ src/main/java/ParallelProgrammingLab2/ParseUtils.java
@@ -5,7 +5,7 @@ public class ParseUtils {
     static String[] ParseFlightsLogLine (String line) {
         String[] parameters = line.split("\",\"");
         //String[] output = {parameters[13], parameters[17]};
-        return new String[] {parameters[13], parameters[17]};
+        return new String[] {parameters[14], parameters[18]};
     }
 
     static String[] ParseAirportsListLine (String line) {
