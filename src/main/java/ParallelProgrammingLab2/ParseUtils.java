447a98e3f4e7a6b8ca5973cf098f4286b4f8fcc0
Mamedov-V-E <black_snake666@mail.ru>
2019-10-17 23:25:33 +0300

    Scripted auto-commit on change (2019-10-17 23:25:33) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/ParseUtils.java
+++ src/main/java/ParallelProgrammingLab2/ParseUtils.java
@@ -3,7 +3,7 @@ package ParallelProgrammingLab2;
 public class ParseUtils {
 
     static String ParseFlightsLogLine (String line) {
-        
+        String[] param
     }
 
     static String ParseAirportsListLine (String line) {
