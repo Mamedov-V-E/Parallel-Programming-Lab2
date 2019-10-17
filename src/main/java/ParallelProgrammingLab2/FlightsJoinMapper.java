29134906a234b052ead825658bcd6afe40198181
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 00:02:50 +0300

    Scripted auto-commit on change (2019-10-18 00:02:50) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/FlightsJoinMapper.java
+++ src/main/java/ParallelProgrammingLab2/FlightsJoinMapper.java
@@ -1,8 +1,19 @@
 package ParallelProgrammingLab2;
 
+import org.apache.hadoop.io.ByteWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] parsedAirportsListLine = ParseUtils.ParseAirportsListLine(value.toString());
+        TextPair outKey = new TextPair(
+                new IntWritable(Integer.parseInt(parsedAirportsListLine[0])), new ByteWritable((byte)0));
+        context.write(outKey, new Text(parsedAirportsListLine[1]));
+    }
 }
