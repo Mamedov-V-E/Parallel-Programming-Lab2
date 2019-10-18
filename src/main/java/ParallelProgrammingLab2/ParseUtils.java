ab7dd2cf5251c26418f717c74ee0939a8b04c883
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 13:16:22 +0300

    Scripted auto-commit on change (2019-10-18 13:16:22) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/ParseUtils.java
+++ src/main/java/ParallelProgrammingLab2/ParseUtils.java
@@ -5,7 +5,7 @@ public class ParseUtils {
     private static final Integer FLIGHTS_LOG_AIRPORT_ID_PARAM_NUMBER = 14;
     private static final Integer FLIGHTS_LOG_DELAY_PARAM_NUMBER = 18;
     public static final String FLIGHTS_LOG_AIRPORT_ID_COLUMN_NAME = "DEST_AIRPORT_ID";
-    public static final String AIRPORTS_AIRPORT_ID_COLUMN_NAME = "";
+    public static final String AIRPORTS_AIRPORT_ID_COLUMN_NAME = "Code,Description";
     static String[] ParseFlightsLogLine (String line) {
         String[] parameters = line.split("\",\"");
         //String[] output = {parameters[13], parameters[17]};
