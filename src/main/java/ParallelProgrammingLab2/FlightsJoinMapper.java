08528c8b12443b94d22fe6caa76a7a5b457417e8
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 22:58:59 +0300

    Scripted auto-commit on change (2019-10-18 22:58:59) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/FlightsJoinMapper.java
+++ src/main/java/ParallelProgrammingLab2/FlightsJoinMapper.java
@@ -12,28 +12,27 @@ public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] parsedFlightsLogLine = ParseUtils.ParseFlightsLogLine(value.toString());
-        if (key.get() > 3 && key.get() <= 100) {
-            System.out.println("Обрабатываю строку номер " + key.get());
-            if (parsedFlightsLogLine[1].isEmpty()) {
-                //System.out.println("Пустая строка в качестве кода аеропорта в строке " + key.get());
-            } else {
-                TextPair outKey = new TextPair(
-                        Integer.parseInt(parsedFlightsLogLine[0]), (byte)1);
-                if (Double.parseDouble(parsedFlightsLogLine[1]) > 0) {
-                    context.write(outKey, new Text(parsedFlightsLogLine[1]));
-                }
-            }
-        } else {
-            System.out.println("номер строки " + key.get());
-        }
-//        if (key.get() > 1) {
-//            if (!parsedFlightsLogLine[1].isEmpty()){
+//        if (key.get() > 0) {
+//            if (parsedFlightsLogLine[1].isEmpty()) {
+//                System.out.println("Пустая строка в качестве кода аеропорта в строке " + key.get());
+//            } else {
 //                TextPair outKey = new TextPair(
 //                        Integer.parseInt(parsedFlightsLogLine[0]), (byte)1);
 //                if (Double.parseDouble(parsedFlightsLogLine[1]) > 0) {
 //                    context.write(outKey, new Text(parsedFlightsLogLine[1]));
 //                }
 //            }
+//        } else {
+//            System.out.println("номер строки " + key.get());
 //        }
+        if (key.get() > 0) {
+            if (!parsedFlightsLogLine[1].isEmpty()){
+                TextPair outKey = new TextPair(
+                        Integer.parseInt(parsedFlightsLogLine[0]), (byte)1);
+                if (Double.parseDouble(parsedFlightsLogLine[1]) > 0) {
+                    context.write(outKey, new Text(parsedFlightsLogLine[1]));
+                }
+            }
+        }
     }
 }
