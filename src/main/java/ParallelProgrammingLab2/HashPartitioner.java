563caff659246e1fa9d2d2ca3483a7b598ee66ba
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 21:05:49 +0300

    Scripted auto-commit on change (2019-10-18 21:05:49) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/HashPartitioner.java
+++ src/main/java/ParallelProgrammingLab2/HashPartitioner.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class HashPartitioner<K, V> extends Partitioner<K, V> {
     @Override
-    public int getPartition(TextPair key, V value, int numReduceTasks) {
-        return (key.getAirportId().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    public int getPartition(K key, V value, int numReduceTasks) {
+        return (((TextPair)key).getAirportId().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
