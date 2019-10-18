cfd4e8dbad07368e5f886e40e2870faddae76240
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 12:53:28 +0300

    Scripted auto-commit on change (2019-10-18 12:53:28) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/ParseUtils.java
+++ src/main/java/ParallelProgrammingLab2/ParseUtils.java
@@ -3,7 +3,8 @@ package ParallelProgrammingLab2;
 public class ParseUtils {
 
     public static final Integer FLIGHTS_LOG_FIRST_PARAM_NUMBER = 14;
-    public static final Integer FLIGHTS_LOG_SECOND_PARAM_NUMBER = 14;
+    public static final Integer FLIGHTS_LOG_SECOND_PARAM_NUMBER = 18;
+    private static final String FLIGHTS_LOG_FIRST_PARAM_COLUMN_NAME
     static String[] ParseFlightsLogLine (String line) {
         String[] parameters = line.split("\",\"");
         //String[] output = {parameters[13], parameters[17]};
