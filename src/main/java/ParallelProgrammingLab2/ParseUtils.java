29858b3932fba4bf1fe847ae89876c7cd2281db8
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 12:51:17 +0300

    Scripted auto-commit on change (2019-10-18 12:51:17) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/ParseUtils.java
+++ src/main/java/ParallelProgrammingLab2/ParseUtils.java
@@ -2,6 +2,7 @@ package ParallelProgrammingLab2;
 
 public class ParseUtils {
 
+    const Integer FLIGHTS_LOG_FIRST_PARAM_NUMBER = 14;
     static String[] ParseFlightsLogLine (String line) {
         String[] parameters = line.split("\",\"");
         //String[] output = {parameters[13], parameters[17]};
