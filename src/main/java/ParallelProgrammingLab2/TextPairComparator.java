0653af1e12827b98045b86e24485864553c4fc7d
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 21:05:13 +0300

    Scripted auto-commit on change (2019-10-18 21:05:13) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/TextPairComparator.java
+++ src/main/java/ParallelProgrammingLab2/TextPairComparator.java
@@ -3,7 +3,7 @@ package ParallelProgrammingLab2;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-public class TextPairComparator extends WritableComparator<TextPair> {
+public class TextPairComparator extends WritableComparator {
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return a.compareTo(b);
