9707e8f44d9998f35c54f0043e66916c2f60fed3
Mamedov-V-E <black_snake666@mail.ru>
2019-10-18 22:02:07 +0300

    Scripted auto-commit on change (2019-10-18 22:02:07) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab2/TextPairComparator.java
+++ src/main/java/ParallelProgrammingLab2/TextPairComparator.java
@@ -3,10 +3,23 @@ package ParallelProgrammingLab2;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
+import java.io.IOException;
+
 public class TextPairComparator extends WritableComparator {
     @Override
     public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-        return compare((WritableComparable)b1, (WritableComparable)b2);
+        try {
+            buffer.reset(b1, s1, l1);                   // parse key1
+            key1.readFields(buffer);
+
+            buffer.reset(b2, s2, l2);                   // parse key2
+            key2.readFields(buffer);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return compare(key1, key2);
     }
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
